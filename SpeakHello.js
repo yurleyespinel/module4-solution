1ebb7df7991087c83de448401b51204425dd2d03
yurleyespinel <61913725+yurleyespinel@users.noreply.github.com>
2020-07-19 13:52:10 -0500

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -5,7 +5,7 @@
 // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
-var helloSpeaker = {}
+var helloSpeaker = {};
 
 // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
 var speakWord = "Hello";
